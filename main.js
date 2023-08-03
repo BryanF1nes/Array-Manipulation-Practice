19c5fd3a501e5f0cec9c3e994ed5e59f237472bb
BryanF1nes <bryanfinesw@gmail.com>
2023-08-03 10:11:51 -0400

    Updated Solutions

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
 // Sum of Array Elements:**
 // Write a function that takes an array of numbers as input and returns the sum of all elements in the array.
-
-const arr = [1, 2, 3, 4, 5];
-
 function sum(arr) {
     return arr.reduce((prev, curr) => prev + curr, 0);
 }
 
 // Reverse Array:**
 // Write a function that takes an array as input and returns a new array with its elements reversed.
-
 function reverse(value) {
     const reversedArray = []
     for (let i = value.length - 1; i >= 0; i--) {
@@ -20,9 +16,10 @@ function reverse(value) {
 
 // Remove Duplicates:**
 // Write a function that takes an array as input and returns a new array with duplicates removed (keeping only the first occurrence of each element).
-const array = [1,2,2,3,3,4,5,5,6]
-
-const uniqueArray = array.filter((value, index) => array.indexOf(value) === index)
+function removeDuplicates() {
+    const duplicates = array.filter((value, index) => array.indexOf(value) === index)
+    return duplicates
+}
 
 
 // Find Maximum and Minimum:**
@@ -38,12 +35,6 @@ function maxAndMin(array) {
 // Count Occurrences:**
 // Write a function that takes an array of elements and a target element as input and returns the count of occurrences of the target element in the array.
 function countOccurrence(array, target) {
-    // array = [1,3,3,5] target = 3 we should have an output of 2
-    // initiate a variable to store count
-    // loop through the array
-        // if value within the array matches the target
-        // increment count++
-    // return count
     let count = 0;
     for (let i = 0; i < array.length; i++) {
         if(target === array[i]) {
@@ -67,12 +58,46 @@ function filterByLength(array, minLength) {
 
 // Capitalize Names:**
 // Write a function that takes an array of names (strings) as input and returns a new array with all names capitalized (the first letter of each name in uppercase).
-const strings = ["mouse", "Cow", "chicken", "Rat"]
-
-const capitalizedNames = strings.map((name) => name.charAt(0).toUpperCase() + name.slice(1))
+function capitalizeNames() {
+    const capitalizedNames = strings.map((name) => name.charAt(0).toUpperCase() + name.slice(1))
+    return capitalizedNames;
+}
 
 // Find Common Elements:**
 // Write a function that takes two arrays as input and returns a new array containing the common elements found in both arrays.
-function commonElements(arr1, arr2) {
-    
+function findCommonElements(arr1, arr2) {
+    const commonElements = arr1.filter((element) => arr2.includes(element));
+    return commonElements;
 }
+
+// Rotate Array:** Write a function that takes an array and a number 'k' as input and rotates the array k steps to the right (positive k) or left (negative k).
+function rotateArray(array, k) {
+    const n = array.length;
+    k = k % n; // Calculate effective number of rotations
+  
+    // Helper function to reverse the elements of the array within the given range
+    function reverse(arr, start, end) {
+      while (start < end) {
+        const temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+      }
+    }
+  
+    // Reverse the entire array
+    reverse(array, 0, n - 1);
+  
+    if (k >= 0) {
+      // Right rotation
+      reverse(array, 0, k - 1);
+      reverse(array, k, n - 1);
+    } else {
+      // Left rotation
+      reverse(array, 0, n + k - 1);
+      reverse(array, n + k, n - 1);
+    }
+  
+    return array;
+  }
